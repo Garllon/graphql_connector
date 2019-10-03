7cb55ca3b4303377ac6ac6405cc22df880013ff5
Garllon <palluthe.bennet@gmail.com>
2019-10-03 18:06:49 +0200

    init

---
+++ lib/graphql_connector.rb
@@ -0,0 +1,6 @@
+require "graphql_connector/version"
+
+module GraphqlConnector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/graphql_connector/version.rb
@@ -0,0 +1,3 @@
+module GraphqlConnector
+  VERSION = "0.1.0"
+end

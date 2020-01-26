38479fd12a4574f8a8655fad161d76447b89cc4d
sushie <sascha_burku@yahoo.de>
2020-01-26 19:16:59 +0100

    Remove obsolete methods

--- lib/graphql_connector.rb
+++ lib/graphql_connector.rb
@@ -27,23 +27,4 @@ module GraphqlConnector
   def self.configure
     yield(configuration)
   end
-
-  def self.query(model, conditions, selected_fields)
-    query_string = QueryBuilder.new(model, conditions, selected_fields).create
-    parsed_body = raw_query(query_string)
-    OpenStruct.new(parsed_body['data'][model])
-  end
-
-  def self.raw_query(query_string)
-    response = HTTParty.post(GraphqlConnector.configuration.host,
-                             headers: GraphqlConnector.configuration.headers,
-                             body: { query: query_string })
-    parsed_body = JSON.parse(response.body)
-
-    if parsed_body.key? 'errors'
-      raise CustomAttributeError, parsed_body['errors']
-    end
-
-    parsed_body
-  end
 end

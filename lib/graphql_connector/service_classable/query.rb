539430d96f30314167c67177f6ba12f4215756c0
Sascha Burku <sascha_burku@yahoo.de>
2020-04-17 11:11:08 +0200

    Provide separated raw_query builder

--- lib/graphql_connector/service_classable/query.rb
+++ lib/graphql_connector/service_classable/query.rb
@@ -33,30 +33,53 @@ module GraphqlConnector
           return query_method(class_method_name, query_type) if params.empty?
 
           ParamsValidator.validate(params)
-          keyword_query_method(class_method_name, query_type, params)
+          query_keyword_method(class_method_name, query_type, params)
+        end
+
+        def raw_query(build_params)
+          class_method_name = build_params.first[0]
+          query_string = build_params.first[1]
+          params = build_params[:params] || build_params['params'] || []
+          ClassMethodValidator.validate(class_method_name, query_string)
+
+          if params.empty?
+            return raw_query_method(class_method_name, query_string)
+          end
+
+          ParamsValidator.validate(params)
+          raw_query_keyword_method(class_method_name, query_string, params)
         end
 
         private
 
-        def query_method(name, query_type)
-          define_singleton_method name do
-            return http_client.raw_query(query_type) if query_type.is_a?(String)
-
+        def query_method(class_method_name, query_type)
+          define_singleton_method class_method_name do
             http_client.query(query_type, {}, @return_fields.to_a)
           end
         end
 
-        def keyword_query_method(name, query_type, params)
-          params = [params].flatten
+        def raw_query_method(class_method_name, query_string)
+          define_singleton_method class_method_name do
+            http_client.raw_query(query_string)
+          end
+        end
+
+        def query_keyword_method(name, query_type, keywords)
+          keywords = [keywords].flatten
           instance_eval <<-METHOD, __FILE__, __LINE__ + 1
-            def #{name}(#{params.map { |keyword| "#{keyword}:" }.join(', ')})
-              if #{query_type.is_a?(String)}
-                http_client
-                  .raw_query("#{query_type}", variables: #{CONDITIONS})
-              else
-                http_client
-                  .query("#{query_type}", #{CONDITIONS}, #{@return_fields.to_a})
-              end
+            def #{name}(#{keywords.map { |keyword| "#{keyword}:" }.join(', ')})
+              http_client.query("#{query_type}",
+                                #{CONDITIONS},
+                                #{@return_fields.to_a})
+            end
+          METHOD
+        end
+
+        def raw_query_keyword_method(name, query_string, keywords)
+          keywords = [keywords].flatten
+          instance_eval <<-METHOD, __FILE__, __LINE__ + 1
+            def #{name}(#{keywords.map { |keyword| "#{keyword}:" }.join(', ')})
+              http_client.raw_query("#{query_string}", variables: #{CONDITIONS})
             end
           METHOD
         end

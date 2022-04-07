c2737dc971cae301906823faad4be6f9b715b3fb
Garllon <palluthe.bennet@gmail.com>
2022-04-07 09:28:50 +0200

    refactor and simplify logic

--- lib/graphql_connector/service_classable/queryable.rb
+++ lib/graphql_connector/service_classable/queryable.rb
@@ -11,44 +11,46 @@ module GraphqlConnector
 
       def add_query(params: [], httparty_adapter_options: {}, returns:, **method_to_query)
         class_method_name = method_to_query.first[0]
-        query_type = method_to_query.first[1]
+        query_type        = method_to_query.first[1]
+        signature         = method_signature(class_method_name, params)
+
         ensure_params_format!(returns, class_method_name, query_type)
 
         if params.empty?
-          return query_method(class_method_name, query_type, returns, httparty_adapter_options)
+          return query_method(signature, query_type, returns, httparty_adapter_options)
         end
 
         ParamsValidator.validate(params)
-        query_keyword_method(class_method_name,
-                             query_type, params, returns, httparty_adapter_options)
+        create_query_method(signature, query_type, returns, httparty_adapter_options)
       end
 
       def add_raw_query(params: [], httparty_adapter_options: {}, **method_to_raw_query)
         class_method_name = method_to_raw_query.first[0]
-        query_string = method_to_raw_query.first[1]
+        query_string      = method_to_raw_query.first[1]
+        signature         = method_signature(class_method_name, params)
+
         ClassMethodValidator.validate_class_method(class_method_name, self)
         ClassMethodValidator.validate_element_class_type(query_string, String)
 
-        if params.empty?
-          return raw_query_method(class_method_name, query_string, httparty_adapter_options)
-        end
+        return raw_query_method(signature, query_string, httparty_adapter_options) if params.empty?
 
         ParamsValidator.validate(params)
-        raw_query_keyword_method(class_method_name, query_string, params, httparty_adapter_options)
+        raw_query_keyword_method(signature, query_string, httparty_adapter_options)
       end
 
       def add_mutation(params: [], httparty_adapter_options: {}, returns:, **method_to_query)
         class_method_name = method_to_query.first[0]
-        query_type = method_to_query.first[1]
+        query_type        = method_to_query.first[1]
+        signature         = method_signature(class_method_name, params)
+
         ensure_params_format!(returns, class_method_name, query_type)
 
         if params.empty?
-          return mutation_method(class_method_name, query_type, returns, httparty_adapter_options)
+          return mutation_method(signature, query_type, returns, httparty_adapter_options)
         end
 
         ParamsValidator.validate(params)
-        mutation_keyword_method(class_method_name,
-                                query_type, params, returns, httparty_adapter_options)
+        create_mutation_method(signature, query_type, returns, httparty_adapter_options)
       end
 
       private
@@ -74,22 +76,9 @@ module GraphqlConnector
         end
       end
 
-      def query_keyword_method(name, query_type, keywords, return_fields, httparty_adapter_options)
-        keywords = [keywords].flatten
+      def raw_query_keyword_method(signature, query_string, httparty_adapter_options)
         instance_eval <<-METHOD, __FILE__, __LINE__ + 1
-          def #{name}(#{keywords.map { |keyword| "#{keyword}:" }.join(', ')})
-            http_client.query("#{query_type}",
-                              #{CONDITIONS},
-                              #{return_fields.to_a},
-                              httparty_adapter_options: #{httparty_adapter_options})
-          end
-        METHOD
-      end
-
-      def raw_query_keyword_method(name, query_string, keywords, httparty_adapter_options)
-        keywords = [keywords].flatten
-        instance_eval <<-METHOD, __FILE__, __LINE__ + 1
-          def #{name}(#{keywords.map { |keyword| "#{keyword}:" }.join(', ')})
+          def #{signature}
             http_client.raw_query("#{query_string}",
                                   variables: #{CONDITIONS},
                                   httparty_adapter_options: #{httparty_adapter_options})
@@ -103,15 +92,38 @@ module GraphqlConnector
         end
       end
 
-      def mutation_keyword_method(name,
-                                  query_type, keywords, return_fields, httparty_adapter_options)
+      def method_signature(name, keywords)
+        return name if keywords.empty?
+
         keywords = [keywords].flatten
+        "#{name}(#{keywords.map { |keyword| "#{keyword}:" }.join(', ')})"
+      end
+
+      def create_mutation_method(signature, query_type, return_fields, httparty_adapter_options)
+        create_keyword_method_for('mutation',
+                                  signature,
+                                  query_type,
+                                  return_fields,
+                                  httparty_adapter_options)
+      end
+
+      def create_query_method(signature, query_type, return_fields, httparty_adapter_options)
+        create_keyword_method_for('query',
+                                  signature,
+                                  query_type,
+                                  return_fields,
+                                  httparty_adapter_options)
+      end
+
+      def create_keyword_method_for(graphql_method_type, signature, query_type, return_fields,
+                                    httparty_adapter_options)
         instance_eval <<-METHOD, __FILE__, __LINE__ + 1
-          def #{name}(#{keywords.map { |keyword| "#{keyword}:" }.join(', ')})
-            http_client.mutation("#{query_type}",
-                                  #{CONDITIONS},
-                                  #{return_fields.to_a},
-                                  httparty_adapter_options: #{httparty_adapter_options})
+          def #{signature}
+            http_client.#{graphql_method_type}(
+              "#{query_type}",
+              #{CONDITIONS},
+              #{return_fields.to_a},
+              httparty_adapter_options: #{httparty_adapter_options})
           end
         METHOD
       end
